fc717a05fde43a70e38d494f10775dd8e8dec208
Emil Jarosiewicz <emiljarosiewicz@gmail.com>
2018-09-13 21:23:29 +0200

    Added extracted app dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 import app.AppSettings
+import app.ApppDependencies
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
 plugins {
@@ -27,11 +28,11 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.2.61")
-    implementation("com.android.support:appcompat-v7:28.0.0-rc02")
-    implementation("com.android.support.constraint:constraint-layout:1.1.3")
-    implementation("com.android.support:design:28.0.0-rc02")
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("com.android.support.test:runner:1.0.2")
-    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    implementation(Dependencies.kotlinStdLib)
+    implementation(ApppDependencies.appCompat)
+    implementation(ApppDependencies.constraintLayout)
+    implementation(ApppDependencies.design)
+    testImplementation(Dependencies.Test.jUnit)
+    androidTestImplementation(Dependencies.UITest.espresso)
+    androidTestImplementation(Dependencies.UITest.runner)
 }
---
+++ buildSrc/src/main/kotlin/app/ApppDependencies.kt
@@ -0,0 +1,14 @@
+package app
+
+object ApppDependencies {
+
+    const val appCompat ="com.android.support:appcompat-v7:${Versions.appCompat}"
+    const val constraintLayout ="com.android.support.constraint:constraint-layout:${Versions.constraintLayout}"
+    const val design  ="com.android.support:design:${Versions.design}"
+
+    object Versions {
+        const val appCompat ="28.0.0-rc02"
+        const val constraintLayout ="1.1.3"
+        const val design  ="28.0.0-rc02"
+    }
+}
